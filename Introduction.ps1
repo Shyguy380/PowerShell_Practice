dbd3c4441e72fa8a5c89e956b83499e544a94571
LoganO <lco37@nau.edu>
2022-07-11 16:51:52 -0700

    Updating Introduction with more work

--- Introduction.ps1
+++ Introduction.ps1
@@ -1,9 +1,32 @@
-﻿"Hello, World!"
+﻿#"Hello, World!"
 
-#$PSVersionTable
+#This is how you may initialize variables in PS
+[string]$dayOfWeek = (Get-Date -Format "dddd")
 
-#Get-ExecutionPolicy
+#If today is Monday
+if($dayOfWeek = "Monday") {
+    
+    #Output Happy Monday
+   "Happy Monday" 
+}
 
-#Get-Command
+#If today is not Monday
+else {
 
+    #Output the lack of it being Monday
+    "It's not Monday"
+}
+
+#Some commands I've explored
+<#
+$PSVersionTable
+
+Get-ExecutionPolicy
+
+Get-Command
+
+& 'C:\Program Files\Sublime Text\sublime_text.exe'
+#>
+
+#Hanging the script tp prevent closing after output
 cmd /c pause
