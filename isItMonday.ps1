90d9b5add5a3954a45997ae6452cae92cadb56cb
LoganO <lco37@nau.edu>
2022-07-13 12:32:21 -0700

    Breaking down the main file

---
+++ isItMonday.ps1
@@ -0,0 +1,18 @@
+﻿#Practicing initializing and doing some basic comparing.
+#This is how you may initialize variables in PS
+[string]$dayOfWeek = (Get-Date -Format "dddd")
+[int]$isMonday = $dayOfWeek.CompareTo("Monday")
+
+#If today is Monday
+if($isMonday = 0) {
+    
+    #Output Happy Monday
+   "Happy Monday" 
+}
+
+#If today is not Monday
+else {
+
+    #Output the lack of it being Monday
+    "It's not Monday"
+}
---
+++ testingCommands.ps1
@@ -0,0 +1,13 @@
+﻿#Some commands I've explored
+<#
+$PSVersionTable
+
+Get-ExecutionPolicy
+
+Get-Command
+
+& 'C:\Program Files\Sublime Text\sublime_text.exe'
+#>
+
+#Hanging the script tp prevent closing after output
+#cmd /c pause
